2a0526166fcb485957df886f20a802192fc653bf
Carlos E <elizaldc@protonmail.com>
2022-05-02 11:37:22 -0500

    Added initial functionality for display of cards and created new file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {useState} from "react";
-import Navbar from "./components/Navbar.js"
+import Navbar from "./components/Navbar.js";
+
 
 function App() {
   const [characters, setCharacters] = useState()
@@ -7,10 +8,52 @@ function App() {
   const [currentScore, setCurrentScore] = useState(0)
   const [clicked, setClicked] = useState(false)
 
+  const handleClick = (id) => {
+    shuffleCharacters()
+    handleScore(id)
+  }
 
+  const handleScore = (id) => {
+    characters.forEach(character => {
+      if (id === character.id && character.clicked === false) {
+        character.clicked = true;
+        setClicked(false)
+      } else if(id === character.id && character.clicked === true) {
+        if(currentScore > highScore) {
+          setHighScore(currentScore)
+        }
+        setCurrentScore(0);
+        setClicked(true);
+        characters.forEach(character => character.clicked = false);
+      }
+    })
+  }
+
+  const shuffleCharacters = () => {
+    const shuffled = shuffle(characters)
+    setCharacters({shuffled})
+  }
+
+  const shuffle = (array) => {
+    let currentIndex = array.length;
+    let temporaryValue = 0;
+    let randomIndex = 0;
+    let newArray = array;
+
+    while (0 !== currentIndex){
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      temporaryValue = newArray[currentIndex];
+      newArray[currentIndex] = newArray[randomIndex];
+      newArray[randomIndex] = temporaryValue;
+    }
+
+    return newArray;
+  }
   return (
     <div>
-      <Navbar />
+      <Navbar score={currentScore} highScore={highScore}/>
     </div>
   );
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,14 @@
 import React from "react";
 
-const Navbar = () => {
+const Navbar = ({score, highScore}) => {
     return(
         <div className="navbar-box">
             <h1 className="navbar-header">Titanfall 2 Memory Game
             <h2 className="navbar-desc">Click on as many original characters as you can!</h2>
             </h1>
             <nav className="scores">
-                <p>Score:</p>
-                <p>High Score: </p>
+                <p>Score: {score}</p>
+                <p>High Score: {highScore} </p>
             </nav>
         </div>
     )
